fc13257d14bb0568dc47f57f4bf13a619718808c
Elliott Anderson <elliott@elliottanderson.org>
2017-05-27 22:03:36 +0100

    Update whereforeArtThou.js

--- intermediateAlgorithms/whereforeArtThou.js
+++ intermediateAlgorithms/whereforeArtThou.js
@@ -4,10 +4,6 @@ function whatIsInAName(collection, source) {
   for (var i = 0; i < collection.length; i++) {
     matches = true;
     for (var prop in source) {
-      console.log(prop);
-      console.log(collection[i].hasOwnProperty(prop));
-      console.log(collection[i][prop] === source[prop]);
-      console.log('-----');
       if (!collection[i].hasOwnProperty(prop) || collection[i][prop] !== source[prop]) {
         matches = false;
       } 
